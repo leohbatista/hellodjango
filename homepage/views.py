5c59e5f282d3cac51a3f963d73d5eda3a63b3ae7
Leonardo Henrique Batista <leonardo@lhbdigital.com.br>
2021-08-01 19:42:08 -0300

    chapter 13

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,13 @@
+import datetime
+from typing import Any, Dict
 from django.views.generic import TemplateView
 
 
 class HomepageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['my_statement'] = f'This message is dynamically set at {datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+        return context
 
